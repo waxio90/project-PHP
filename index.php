e01290c738edc488a55edb29dd8070ee87255a15
Wojciech Krupiński <44066436+waxio90@users.noreply.github.com>
2021-08-04 11:18:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,18 +20,18 @@ $configuration = require_once("config/config.php");
 $request = new Request($_GET, $_POST);
 
 
-// try {
-    Controller::initConfiguration($configuration);
-    (new Controller($request))->run();
+try {
+  Controller::initConfiguration($configuration);
+  (new Controller($request))->run();
     
-// } catch (ConfigurationException $e) {
-//     echo '<h1>Wystąpił błąd w aplikacji</h1>';
-//     echo 'Problem z konfiguracją';
-// } catch (AppException $e) {
-//     echo '<h1>Wystąpił błąd w aplikacji</h1>';
-//     echo '<h3>' . $e->getMessage() . '</h3>';
-// } catch (\Throwable $e) {
-//     echo '<h1>Wystąpił błąd w aplikacji</h1>';
-// }
+} catch (ConfigurationException $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo 'Problem z konfiguracją';
+} catch (AppException $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (\Throwable $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+}
 
 
