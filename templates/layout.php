3553c77c901eaee3914d3cb524cd2be1c1a3c684
waxio <krupinski1@gmail.com>
2021-10-02 16:22:14 +0200

    update file

--- templates/layout.php
+++ templates/layout.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
-        <link href="/project/public/style.css" rel="stylesheet">
+        <link href="public/style.css" rel="stylesheet">
     </head>
     <body>
     	<header>
