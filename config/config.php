ede8373c8bbaf59436b21437821174b42135fd78
waxio <krupinski1@gmail.com>
2021-10-02 16:23:00 +0200

    update file

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'database' => 'it_job_offers',
+        'user' => 'user_it_job_offers',
+        'password' => '123456'
+    ]
+];
